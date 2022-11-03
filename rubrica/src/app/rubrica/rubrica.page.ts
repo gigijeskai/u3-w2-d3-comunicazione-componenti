70fc44bf4e3b52b8d7d003cd1b9e4e721707815f
Luigi Indaco <luigindaco@gmail.com>
2022-11-03 17:00:45 +0100

    consegna

--- rubrica/src/app/rubrica/rubrica.page.ts
+++ rubrica/src/app/rubrica/rubrica.page.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './rubrica.page.html',
-  styleUrls: ['./rubrica.page.scss']
+  styleUrls: ['./rubrica.page.scss'],
 })
 export class RubricaPage implements OnInit {
+  listaContatti = [
+    { nome: 'mario', cognome: 'rossi', telefono: 1234567890 },
+    { nome: 'maria', cognome: 'rosi', telefono: 1234567899 },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- rubrica/src/app/rubrica/tabella/tabella.component.ts
+++ rubrica/src/app/rubrica/tabella/tabella.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { RubricaPage } from '../rubrica.page';
 
 @Component({
   selector: 'app-tabella',
   templateUrl: './tabella.component.html',
-  styleUrls: ['./tabella.component.scss']
+  styleUrls: ['./tabella.component.scss'],
 })
 export class TabellaComponent implements OnInit {
+  @Input() contatti: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
